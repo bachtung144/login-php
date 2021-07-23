d1ba282c14fa8a7539aa7fc14ef0c3a07f74d2b3
bachtung144 <bachtung144007@gmail.com>
2021-07-23 09:26:27 +0700

    add db user

--- bootstrap.php
+++ bootstrap.php
@@ -3,3 +3,4 @@ require_once 'app/App.php';
 require_once 'configs/routes.php';
 require_once 'core/Controller.php';
 require_once 'core/Route.php';
+require_once 'configs/conn.php';
---
+++ configs/conn.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect("127.0.0.1","root","","sample");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+//else{
+//    echo "Connected successfully 1";
+//}
+?>
